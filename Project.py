431cd8cf5ef9f7d6a03e70fc8d856e73d0bb47c9
FookNaRak <66070098@kmitl.ac.th>
2023-10-01 18:11:51 +0700

    [build]add pandas module to read excel file

--- Project.py
+++ Project.py
@@ -1,12 +1,12 @@
 """PSCP project"""
-def bmr():
-    sex = input("Enter your gender(Male/Female) : ")
-    age = int(input("Enter your age : "))
-    weight = float(input("Enter your weight(KG) : "))
-    height = float(input("Enter Your height(CM) : "))
+import pandas as pd
+def bmr(sex, age, height, weight):
     if sex == "Male":
         result = 66 + (13.7 * weight) + (5 * height) - (6.8 * age)
     elif sex == "Female":
         result = 665 + (9.6 * weight) + (1.8 * height) - (4.7 * age)
     return result
-bmr()
+def test():
+    df = pd.read_excel(r'food_data.xlsx')
+    print(df)
+test()
